6338533e0e7d8b9414743edef099d4fffa271094
Fernando Rincon <frm.rincon@gmail.com>
2014-05-20 19:21:37 -0500

    Added hibernte3-maven-plugin to external dependencies.

--- external-dependencies/install.sh
+++ external-dependencies/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cd hibernate-tools && (mvn clean install > /dev/null 2>&1 )&& cd .. 
+cd hibernate-tools && (mvn clean install > /dev/null 2>&1 ) && cd .. 
+cd hibernate3-maven-plugin-3.0 && (mvn clean install > /dev/null 2>&1 ) && cd ..
